4d3d05f468d2704367b5fcf0e5b59d59eb8874ea
TuInh2310Automation <tuinhle2310@gmail.com>
2023-05-25 15:35:19 +0700

    TestNG-description

---
+++ src/testng/Topic_03_description.java
@@ -0,0 +1,17 @@
+
+package testng;
+
+import org.testng.annotations.Test;
+
+public class Topic_03_description {
+	
+	@Test(description = "JIRA 878888 Create employee and verify employee created successfully ")
+	public void EndUser_Register_new_Employee() {
+		
+	}
+
+	@Test(enabled = false)
+	public void EndUser_View_Employee() {	
+	}
+
+}
